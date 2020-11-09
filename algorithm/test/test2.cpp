6e149774c3bf4408e5eb3e30199d3c72f909b808
Yunner-tt <734270075@qq.com>
2020-11-09 22:27:41 +0800

    目录调整

---
+++ algorithm/test/test2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int content[4][5]={0};
+    for (int i = 0; i < 3; ++i)
+    {
+        for (int j = 0; j < 4; ++j)
+        {
+            cin >> content[i][j];
+        }
+    }
+    for (int i = 0; i < 3; ++i)
+    {
+        for (int j = 0; j < 4; ++j)
+        {
+            content[i][4]+=content[i][j];
+            content[3][j]+=content[i][j];
+            cout<<content[i][j]<<"  ";
+        }
+        cout<<"row_sum:" <<content[i][4]<<endl;
+    }
+    cout<<"colum_sum:"<<endl;
+    cout<<content[3][0]<<"  "<<content[3][1]<<"  "<<content[3][2]<<"  "<<content[3][3]<<endl;
+    system("pause");
+    return 0;
+}
